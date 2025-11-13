44cdfb9713a2250aecb102dad46ae80fecb263cd
carolSu08 <carolineesuzart@gmail.com>
2025-11-13 12:50:47 -0300

    Add files via upload

---
+++ SistemaCadastrarAluno/src/Conexao.java
@@ -0,0 +1,15 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexao {
+    public static Connection getConnetion(){
+        try{
+            String url = "jdbc:mysql: //localhost:3306/cadastro";
+            String user = "root"; // troque pelo seu usuário
+            String password = "1234"; // troque pela sua senha
+            return DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro na conexão: " + e.getMessage());
+        }
+    }
+}
--- SistemaCadastrarAluno/src/JanelaPrincipal.java
+++ SistemaCadastrarAluno/src/JanelaPrincipal.java
@@ -1,16 +1,24 @@
+// exemplo 2 //
+
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.*;
+import java.util.List;
 
 public class JanelaPrincipal extends JFrame {
     private JTextField campoNome, campoEmail, campoRua, campoCidade;
-    private JComboBox<String>comboCurso;
+    private JComboBox<String> comboCurso;
     private JCheckBox checkEmail, checkNotificacao;
     private JRadioButton radioMasc, radioFem;
-    private JButton btnCadastrar, btnLimpar, btnSair;
+    private JButton btnCadastrar, btnLimpar;
+
+    private JTable tabela;
+    private DefaultTableModel modeloTabela;
 
     public JanelaPrincipal(){
         setTitle("Sistema de Cadastro de Alunos");
-        setSize(500,400);
+        setSize(700,500);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
@@ -23,7 +31,7 @@ public class JanelaPrincipal extends JFrame {
 
         JMenu menuAjuda = new JMenu("Ajuda");
         JMenuItem itemSobre = new JMenuItem("Sobre");
-        itemSobre.addActionListener(e-> JOptionPane.showMessageDialog(this,"Sisyema de Cadastro de Alunos\nversão 1.0"));
+        itemSobre.addActionListener(e-> JOptionPane.showMessageDialog(this,"Sisyema de Cadastro de Alunos\nCRUD Completo\n versão 2.0"));
         menuAjuda.add(itemSobre);
 
         barra.add(menuArquivo);
@@ -34,32 +42,118 @@ public class JanelaPrincipal extends JFrame {
         JTabbedPane abas = new JTabbedPane();
 
         // Painel Dados Pessoais
-        JPanel painelDados = new JPanel(new GridLayout(6,2));
-        painelDados.add(new JLabel("Nome:"));
+        JPanel painelCadastro = new JPanel(new GridLayout(7,2));
+        painelCadastro.add(new JLabel("Nome:"));
         campoNome = new JTextField(20);
-        painelDados.add(campoNome);
+        painelCadastro.add(campoNome);
 
-        painelDados.add(new JLabel("Email"));
+        painelCadastro.add(new JLabel("Email"));
         campoEmail = new JTextField(20);
-        painelDados.add(campoEmail);
+        painelCadastro.add(campoEmail);
 
-        painelDados.add(new JLabel("Curso"));
+        painelCadastro.add(new JLabel("Curso"));
         String[] cursos = {"java", "Python","C#", "JavaScript"};
         comboCurso = new JComboBox<>(cursos);
-        painelDados.add(comboCurso);
+        painelCadastro.add(comboCurso);
 
-        painelDados.add(new JLabel("Genero"));
+        painelCadastro.add(new JLabel("Genero"));
         JPanel painelGenero = new JPanel();
         radioMasc = new JRadioButton("Masculino");
         radioMasc = new JRadioButton("Feminino");
-
         ButtonGroup grupoGenero = new ButtonGroup();
         grupoGenero.add(radioMasc);
         grupoGenero.add(radioFem);
         painelGenero.add(radioMasc);
         painelGenero.add(radioFem);
-        painelDados.add(pain)
+        painelCadastro.add(painelGenero);
 
+        checkEmail = new JCheckBox("Receber emails");
+        checkNotificacao = new JCheckBox("Ativar notificações");
+        painelCadastro.add(checkEmail);
+        painelCadastro.add(checkNotificacao);
+
+        abas.add("Dados Pessoais", painelDados);
+
+        // Painel Endereço
+        JPanel painelEndereco = new JPanel(new GridLayout(2,2));
+        painelEndereco.add(new JLabel("Rua:"));
+        campoRua = new JTextField(20);
+        painelEndereco.add(campoRua);
+        painelEndereco.add(new JLabel("Cidade:"));
+        campoCidade = new JTextField(20);
+        painelEndereco.add(campoCidade);
+        abas.add("Endereço", painelEndereco);
+
+        // Botões
+        JPanel painelBotoes = new JPanel();
+        btnCadastrar = new JButton("Cadastrar");
+        btnLimpar = new JButton("Limpar");
+        btnSair = new JButton("Sair");
+        painelBotoes.add(btnCadastrar);
+        painelBotoes.add(btnLimpar);
+        painelBotoes.add(btnSair);
+
+        // Ações dos botões
+        btnCadastrar.addActionListener(e -> cadastrarAluno());
+        btnLimpar.addActionListener(e -> limparCampos());
+        btnSair.addActionListener(e -> System.exit(0));
+
+        // Layout principal
+        getContentPane().setLayout(new BorderLayout());
+        getContentPane().add(abas, BorderLayout.CENTER);
+        getContentPane().add(painelBotoes, BorderLayout.SOUTH);
+        setVisible(true);
+    }
+    private void cadastrarAluno() {
+        Aluno aluno = new Aluno();
+        aluno.setNome(campoNome.getText());
+        aluno.setEmail(campoEmail.getText());
+
+        aluno.setCurso((String) comboCurso.getSelectedItem());
+        aluno.setGenero(radioMasc.isSelected() ? "Masculino" : "Feminino");
+        aluno.setReceberEmail(checkEmail.isSelected());
+        aluno.setReceberNotificacao(checkNotificacao.isSelected());
+        aluno.setRua(campoRua.getText());
+        aluno.setCidade(campoCidade.getText());
+        JOptionPane.showMessageDialog(this,
+                "Aluno cadastrado com sucesso:\n" +
+                        "Nome: " + aluno.getNome() + "\n" +
+                        "Email: " + aluno.getEmail() + "\n" +
+                        "Curso: " + aluno.getCurso() + "\n" +
+                        "Gênero: " + aluno.getGenero() + "\n" +
+                        "Rua: " + aluno.getRua() + "\n" +
+                        "Cidade: " + aluno.getCidade());
+
+        private void cadastrarAluno(){
+            Aluno aluno1 = new Aluno();
+            aluno.setNome(campoNome.getText());
+            aluno.setEmail(campoEmail.getText());
+            aluno.setCurso((String) comboCurso.getSelectedItem());
+            aluno.setGenero(radioMasc.isSelected() ? "Masculino" : "Feminino");
+            aluno.setReceberEmail(checkEmail.isSelected());
+            aluno.setReceberNotificacao(checkNotificacao.isSelected());
+            aluno.setRua(campoRua.getText());
+            aluno.setCidade(campoCidade.getText());
+
+            AlunoDAO dao = new AlunoDAO();
+            dao.salvar(aluno);
+
+            JOptionPane.showMessageDialog(this,"Aluno cadastrado com sucesso no banco de dados!");
+        }
+    }
+    private void limparCampos() {
+        campoNome.setText("");
+        campoEmail.setText("");
+        comboCurso.setSelectedIndex(0);
+        radioMasc.setSelected(false);
+        radioFem.setSelected(false);
+        checkEmail.setSelected(false);
+        checkNotificacao.setSelected(false);
+        campoRua.setText("");
+        campoCidade.setText("");
+    }
+    public static void main(String[] args) {
+        new JanelaPrincipal();
     }
 }
 
